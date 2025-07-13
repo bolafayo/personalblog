16f2d6e7b1d78111ea4c617f018f31a99db52fd8
Bolade Olufayo Taiwo <olufayobolade@gmail.com>
2025-07-13 13:02:10 +0100

    store the form in local storage

--- app.js
+++ app.js
@@ -15,6 +15,45 @@
                 }
             });
         });
+function toggleShare() {
+  const shareIcons = document.getElementById("share-icons");
+  shareIcons.classList.toggle("hidden");
+}
+
+function copyLink() {
+  navigator.clipboard.writeText(window.location.href).then(() => {
+    alert("Link copied to clipboard!");
+  });
+}
+
+  function handlesubribe(event) {
+    event.preventDefault()
+
+    const user = []
+
+  const firstName = document.getElementById ('fristName')
+  const lastName = document.getElementById('lastName')
+  const eamil = document.getElementById('email')
+  const subscribe = document.getElementById('subscribeConsent')
+
+  if(!subscribe){
+    alert("You must agree to subscribe")
+    return;
+  }
+  const subscriber ={
+    firstName,
+    lastName,
+    eamil,
+  }
+  const existing = JSON.parse(localStorage.getItem("subscriber")) || [];
+  existing.push(subscribe)
+
+  localStorage.setItem("subscriber") , JSON.stringify(existing)
+
+   alert(`Thanks for subscribing, ${firstName}!`);
+  document.getElementById("newsletter-form").reset();
 
 
+
+  }
   
